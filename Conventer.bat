1eb3dffff09ecbeadc4bee9b86a11733a384b7ae
Alexandr <smirnov.rus.msk@gmail.com>
2022-05-05 11:09:24 +0300

    Delete Conventer.bat

--- Conventer.bat
+++
@@ -1,4 +0,0 @@
-"C:\Users\Alex1\AppData\Local\Programs\Python\Python38\python.exe" "C:\Python\Converter\60m_converter.py"
-"C:\Users\Alex1\AppData\Local\Programs\Python\Python38\python.exe" "C:\Python\Converter\30m_converter.py"
-"C:\Users\Alex1\AppData\Local\Programs\Python\Python38\python.exe" "C:\Python\Converter\15m_converter.py"
-"C:\Users\Alex1\AppData\Local\Programs\Python\Python38\python.exe" "C:\Python\Converter\EODconverter.py"
